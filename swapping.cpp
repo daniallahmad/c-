f4a564fee4f206f35780723530bb7a9468e494da
Daniall Ahmad <daniallahmad@outlook.com>
2022-04-08 16:37:07 +0500

    Add files via upload

---
+++ swapping.cpp
@@ -0,0 +1,35 @@
+/**
+	CS-1A, C++ Practice
+    Purpose: Swaps the values of two variables with each other using a third variable 'temp'.
+    @file swapping.cpp
+	
+    @author Daniall Ahmad
+    @date 05/04/22 
+	
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+    int temp;
+    
+	cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    
+	cout << "value of num1 before swapping is: " << num1 << endl;
+    cout << "value of num2 before swapping is: " << num2 << endl;
+	
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    
+	cout << "value of num1 after swapping is: " << num1 << endl;
+    cout << "value of num2 after swapping is: " << num2 << endl;
+    
+	return 0;
+}
