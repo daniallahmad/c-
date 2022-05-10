d7f0ef24ecc1b46a573857cc24f139bece24479d
Daniall Ahmad <daniallahmad@outlook.com>
2022-05-10 14:05:38 +0500

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,11 +1,11 @@
 /**
-	CS-1A, C++ Practice
-	Purpose: Program that accepts a positive and displays the factorial of the value.
-	@file factorial.cpp
-	
-	@author Daniall Ahmad
-	@date 15/04/22 
-	
+    CS-1A, C++ Practice
+    Purpose: Program that accepts a positive and displays the factorial of the value.
+    @file factorial.cpp
+
+    @author Daniall Ahmad
+    @date 15/04/22
+
 */
 
 #include <iostream>
@@ -13,17 +13,24 @@
 using namespace std;
 int main()
 {
-	int value;
-	int factorial = 1;
-	cout << "Enter a positive value : ";
-	cin >> value;
+    int value;
+    int factorial = 1;
+    cout << "Enter a positive value : ";
+    cin >> value;
 
-	for (int loop = value; loop>1; loop--)
-	{
-		factorial *= loop;
-	}
-	cout << "Factorial of value " <<value<<" is : "<<factorial<<endl;
-	
-	_getch();
-	return 0;
+    if (!(value > 1))
+    {
+        cout << "Please enter a positive value." << endl;
+    }
+    else
+    {
+        for (int loop = value; loop > 1; loop--)
+        {
+            factorial *= loop;
+        }
+        cout << "Factorial of value " << value << " is : " << factorial << endl;
+    }
+
+    _getch();
+    return 0;
 }
