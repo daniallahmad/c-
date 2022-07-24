f252706ab58902651c26f88c331c3cb34f6de8d2
Daniall Ahmad <daniallahmad@outlook.com>
2022-07-24 22:56:11 +0500

    Add files via upload

---
+++ linear_search.cpp
@@ -0,0 +1,42 @@
+/**
+	CS-1A, C++ Practice
+	Purpose: Write a C++ program to find a number in a given array of integers using Linear Search.
+	@file linear_search.cpp
+
+	@author Daniall Ahmad
+	@date 24/07/22
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int findIndex(int arr[], int n);
+
+int main()
+{
+	int arr[10] = {80, 10, 18, 29, 98, 37, 1, 35, 46, 76};
+	int number = 76;
+	int check = findIndex(arr, number);
+	if (check == -1)
+	{
+		cout << "Number does not exist.";
+	}
+	else
+	{
+		cout << "Number is present at index: " << check;
+	}
+	return 0;
+}
+
+int findIndex(int arr[], int number)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr[i] == number)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
